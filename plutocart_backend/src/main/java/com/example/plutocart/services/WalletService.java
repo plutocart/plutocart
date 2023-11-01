3c7ab06ea1f6d2af85ba4d2fb08ad1f59171b023
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2023-11-02 01:45:40 +0700

    set up cd

--- plutocart_backend/src/main/java/com/example/plutocart/services/WalletService.java
+++ plutocart_backend/src/main/java/com/example/plutocart/services/WalletService.java
@@ -26,7 +26,7 @@ public class WalletService {
      public void crateWallet(@RequestBody Wallet wallet) throws Exception {
 //            UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal(); // wait login
          //        walletRepository.insertWalletByAccountID(wallet.getNameWallet(), wallet.getBalanceWallet(), accountRepository.findById(2).get().getId(), LocalDateTime.now(), LocalDateTime.now()); // account customer
-        walletRepository.insertWalletByAccountID(wallet.getNameWallet(), wallet.getBalanceWallet(), accountRepository.findById(1).get().getId(), LocalDateTime.now(), LocalDateTime.now()); // account gues
+        walletRepository.insertWalletByAccountID(wallet.getNameWallet(), wallet.getBalanceWallet(), accountRepository.findById(1).get().getId(), LocalDateTime.now(), LocalDateTime.now()); // account guest
 
     }
 
