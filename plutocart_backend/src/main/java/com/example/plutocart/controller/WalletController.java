33b0ca900eac445ca4bcb58b5d7ecc54bc1e780a
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2023-11-01 22:32:25 +0700

    set up cd

--- plutocart_backend/src/main/java/com/example/plutocart/controller/WalletController.java
+++ plutocart_backend/src/main/java/com/example/plutocart/controller/WalletController.java
@@ -4,13 +4,11 @@ import com.example.plutocart.services.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api")
+@CrossOrigin(origins = "*" , allowedHeaders = "*")
 public class WalletController {
 
     @Autowired
