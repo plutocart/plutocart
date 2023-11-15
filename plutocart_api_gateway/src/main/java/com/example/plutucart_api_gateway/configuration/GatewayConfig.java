310ba2b385359eb7c0fe2789c90ebddedc53d173
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2023-11-15 16:30:55 +0700

    update mysql

--- plutocart_api_gateway/src/main/java/com/example/plutucart_api_gateway/configuration/GatewayConfig.java
+++ plutocart_api_gateway/src/main/java/com/example/plutucart_api_gateway/configuration/GatewayConfig.java
@@ -9,22 +9,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("api_route", r -> r.path("/api/**")
-//                        .filters(GatewayFilterSpec::secureHeaders)
-//                        .uri("http://backend:8080"))
-//                // Add more routes as needed
-//                .build();
-//    }
-
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("api_route", r -> r.path("/api/**")
                         .filters(GatewayFilterSpec::secureHeaders)
-                        .uri("http://localhost:8080"))
+                        .uri("http://backend:8080"))
+                // Add more routes as needed
                 .build();
     }
+
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("api_route", r -> r.path("/api/**")
+//                        .filters(GatewayFilterSpec::secureHeaders)
+//                        .uri("http://localhost:8080"))
+//                .build();
+//    }
 }
