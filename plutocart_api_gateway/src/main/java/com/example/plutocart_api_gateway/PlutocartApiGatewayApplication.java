4d1e2fc7a3424124431c88b224ccfa94116b677e
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2023-11-17 16:55:12 +0700

    test ssl

--- plutocart_api_gateway/src/main/java/com/example/plutocart_api_gateway/PlutocartApiGatewayApplication.java
+++ plutocart_api_gateway/src/main/java/com/example/plutocart_api_gateway/PlutocartApiGatewayApplication.java
@@ -1,15 +1,17 @@
 package com.example.plutocart_api_gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class PlutocartApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlutocartApiGatewayApplication.class, args);
+
+		 new SpringApplicationBuilder(PlutocartApiGatewayApplication.class)
+				 .web(WebApplicationType.NONE)
+				 .run(args);
 	}
-
-
-
 }
