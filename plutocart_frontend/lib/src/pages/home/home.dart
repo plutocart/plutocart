a3eb520230f416df438cc8e7b5b650b5c0cdf6a1
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2023-12-10 00:47:59 +0700

    edit comfig skeletion

--- plutocart_frontend/lib/src/pages/home/home.dart
+++ plutocart_frontend/lib/src/pages/home/home.dart
@@ -50,6 +50,7 @@ class _HomePageState extends State<HomePage> {
       body: BlocBuilder<WalletBloc, WalletState>(
         builder: (context, state) {
           return Skeletonizer(
+            containersColor: Colors.grey.shade100,
             enabled: state.isLoading,
             ignoreContainers: false,
             child: SingleChildScrollView(
