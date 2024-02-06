64728f2e5bb87991adb9cc3db777d5b7c5802f24
ChisanuchaK <chisanucha.kim@mail.kmutt.ac.th>
2024-02-06 20:37:30 +0700

    test env backend 2

--- plutocart_api_gateway/src/main/java/com/example/plutocart_api_gateway/filter/UserAgentFilter.java
+++ plutocart_api_gateway/src/main/java/com/example/plutocart_api_gateway/filter/UserAgentFilter.java
@@ -1,7 +1,6 @@
 package com.example.plutocart_api_gateway.filter;
 
-import com.example.plutocart_api_gateway.model.HeaderModel;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpStatus;
@@ -9,16 +8,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserAgentFilter extends AbstractGatewayFilterFactory<UserAgentFilter.Config> {
+
+    @Value("${HEADER_KEY}")
+    private String headerKey;
+    @Value("${VALUE_HEADER}")
+    private String headerValue;
     public UserAgentFilter() {
         super(Config.class);
     }
-    @Autowired
-    HeaderModel headerModel;
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            String userAgent = exchange.getRequest().getHeaders().getFirst(headerModel.headerKey);
-            if (userAgent != null && userAgent.contains(headerModel.headerValue)) {
+            String userAgent = exchange.getRequest().getHeaders().getFirst(headerKey);
+            if (userAgent != null && userAgent.contains(headerValue)) {
                 return chain.filter(exchange);
             } else {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
